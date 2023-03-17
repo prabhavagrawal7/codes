8dada9dec97ff4fdf514745fc295fe0a3251a546
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2023-03-17 19:06:13 +0530

    ordered_set

--- ordered_set.cpp
+++ ordered_set.cpp
@@ -9,32 +9,26 @@ using namespace std;
 // #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 
 // Uncomment them for optimisations
-// #pragma GCC optimize("Ofast")
-// #pragma GCC target("avx2")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
 
 // for segment tree
 // #define mid (start+end)/2
 // #define lnode (node*2+1)
 // #define rnode (node*2+2)
 #define popcount(x) __builtin_popcountll(x)
-#define clz(x) (63 - __builtin_clzll(x)) // count leading zeros
-#define ctz(x) __builtin_ctzll(x)        // count trailing zeros
+#define clz(x) (63-__builtin_clzll(x)) //count leading zeros
+#define ctz(x) __builtin_ctzll(x) //count trailing zeros
 #define GET_MACRO(_1, _2, _3, _4, NAME, ...) NAME
-#define range(...) GET_MACRO(__VA_ARGS__, r4, r3, r2, r1)(__VA_ARGS__)
-#define r4(var, start, stop, step) for (ll var = start; step > 0 ? var < stop : var > stop; var = var + step)
-#define r3(var, start, stop) for (ll var = start; var < stop; ++var)
-#define r2(var, stop) for (ll var = 0; var < stop; ++var)
-#define r1(stop) for (ll start_from_0 = 0; start_from_0 < stop; ++start_from_0)
-#define newint(...) \
-    ll __VA_ARGS__; \
-    take_input(__VA_ARGS__)
+#define range(...) GET_MACRO(__VA_ARGS__, r4, r3, r2, r1) (__VA_ARGS__)
+#define r4(var,start,stop,step) for(ll var=start;step>0?var<stop:var>stop;var=var+step)
+#define r3(var,start,stop) for(ll var=start;var<stop;++var)
+#define r2(var,stop) for(ll var=0;var<stop;++var)
+#define r1(stop) for (ll start_from_0=0;start_from_0<stop; ++start_from_0)
+#define newint(...) ll __VA_ARGS__;take_input(__VA_ARGS__)
 #define min(...) min({__VA_ARGS__})
 #define max(...) max({__VA_ARGS__})
-#define give(...)           \
-    {                       \
-        print(__VA_ARGS__); \
-        return;             \
-    }
+#define give(...){print(__VA_ARGS__);return;}
 #define endl "\n"
 #define FULL_INF numeric_limits<double>::infinity()
 #define INF INT64_MAX
@@ -57,90 +51,30 @@ using namespace std;
 #define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define all(a) a.begin(), a.end()
 #define db(x) cout << #x << " = " << x << "\n"
-string db_bin(ll n)
-{
-    string ans;
-    while (n)
-    {
-        ans.push_back((n & 1) + '0');
-        n >>= 1;
-    }
-    reverse(all(ans));
-    return ans;
-}
-#define newstring(str) \
-    string str;        \
-    cin >> str;
+string db_bin(ll n){string ans;while (n){ans.push_back((n & 1) + '0');n >>= 1;}reverse(all(ans));return ans;}
+#define newstring(str) string str; cin >> str;
 #define foreach(a, x) for (auto &&a : x)
-const ld pi = acos(-1);
-typedef vector<string> vs;
-typedef pair<ll, ll> pii;
-typedef vector<ll> vi;
-typedef map<ll, ll> mii;
-typedef set<ll> si;
-typedef vector<vector<ll>> vvi;
-template <typename... T>
-inline void take_input(T &&...args) { ((cin >> args), ...); }
-vi inputvec(ll n, ll start = 0)
-{
-    vi vec(n);
-    range(i, start, n) cin >> vec[i];
-    return vec;
-}
-template <typename T>
-inline bool btn(T a, T b, T c)
-{
-    if ((a <= b && b <= c) || (a >= b && b >= c))
-        return true;
-    return false;
-}
-template <typename T>
-istream &operator>>(istream &is, V<T> &v)
-{
-    range(i, v.size()) { is >> v[i]; }
-    return is;
-}
-template <typename T>
-ostream &operator<<(ostream &os, const V<T> &v)
-{
-    range(i, v.size()) { os << v[i] << (i + 1 != v.size() ? " " : ""); }
-    return os;
-}
-template <typename _A, typename _B>
-ostream &operator<<(ostream &os, const pair<_A, _B> &p)
-{
-    os << "[" << p.first << ", " << p.second << "]";
-    return os;
-}
-template <typename... T>
-inline void print(T &&...args)
-{
-    ((cout << args << " "), ...);
-    cout << endl;
-}
+const ld pi = acos(-1);typedef vector<string>vs;typedef pair<ll,ll>pii;
+typedef vector<ll>vi;typedef map<ll,ll>mii;typedef set<ll>si;typedef vector<vector<ll>>vvi;
+template<typename...T>inline void take_input(T &&...args) { ((cin >> args), ...); }
+vi inputvec(ll n,ll start=0){vi vec(n);range(i,start,n)cin>>vec[i];return vec;}
+template <typename T>inline bool btn(T a,T b,T c){if ((a <= b && b <= c) || (a >= b && b >= c))return true;return false;}
+template<typename T>istream&operator>>(istream&is,V<T>&v){range(i,v.size()){is>>v[i];}return is;}
+template<typename T>ostream&operator<<(ostream&os,const V<T>&v){range(i,v.size()){os<<v[i]<<(i+1!=v.size()?" ":"");}return os;}
+template<typename _A,typename _B>ostream &operator<<(ostream &os,const pair<_A,_B>&p)
+{os<<"["<<p.first<<", "<<p.second<<"]";return os;}
+template<typename...T>inline void print(T &&...args){((cout << args << " "), ...);cout << endl;}
 template <typename... T>
 inline void printl(T &&...args) { ((cout << args << " "), ...); }
 inline ld TLD(ll n) { return n; }
 ll gcd(ll __m, ll __n) { return __n == 0 ? __m : gcd(__n, __m % __n); }
 const ll mod = 1000000007;
 // const ll mod = 998244353;
-inline ll rs(ll n) { return (n %= mod) >= 0 ? n : n + mod; }
-// define rll above this
+inline ll rs(ll n) { return (n%=mod) >= 0 ? n : n + mod; }
+//define rll above this
 #ifndef __RLL__
-ll power(ll x, ll y)
-{
-    x %= mod, y %= mod - 1;
-    ll res = 1;
-    while (y)
-    {
-        if (y & 1LL)
-            res = (res * x) % mod;
-        y >>= 1;
-        x = (x * x) % mod;
-    }
-    return res % mod;
-}
-ll inv(ll n) { return power(n, mod - 2); }
+ll power(ll x, ll y){x%=mod,y%=mod-1;ll res=1;while(y){if(y&1LL)res=(res*x)%mod;y>>=1;x=(x*x)%mod;}return res%mod;}
+ll inv(ll n){return power(n, mod - 2);}
 #endif
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------- */
 struct ordered_set
@@ -157,17 +91,18 @@ struct ordered_set
         tree() : left(nullptr), right(nullptr), val(0) {}
     };
     tree *node;
+    ordered_set() : node(nullptr) {}
     ll __size = 0;
     size_t size()
     {
         return __size;
     };
-    ll initstart = 0, initend = 1e9;
-    ll insert(ll n)
+    ll initstart = 0, initend = 2e5+1;
+    void insert(ll n)
     {
         insert(n, node, initstart, initend);
     }
-    ll insert(ll n, tree *&node, ll start, ll end)
+    void insert(ll n, tree *&node, ll start, ll end)
     {
         if (node == nullptr)
         {
@@ -199,8 +134,9 @@ struct ordered_set
             return;
         if (start == end)
         {
-            __size = 0;
+            __size -= 1;
             delete node;
+            node = nullptr;
             return;
         }
         if (n <= mid)
@@ -209,13 +145,16 @@ struct ordered_set
             erase(n, rnode, mid + 1, end);
         node->val = lval + rval;
         if (node->val == 0)
+        {
             delete node;
+            node = nullptr;
+        }
     }
 
     ll operator[](ll n)
     {
         if (n < __size)
-            return find_by_order(n, node, initstart, initend);
+            return find_by_order(n + 1, node, initstart, initend);
         else
         {
             print("Bad key exception");
@@ -229,7 +168,7 @@ struct ordered_set
         if (n <= lval)
             return find_by_order(n, lnode, start, mid);
         else
-            return find_by_order(n - lval, lnode, mid + 1, end);
+            return find_by_order(n - lval, rnode, mid + 1, end);
     }
 
     ll find_order(ll n)
@@ -247,9 +186,10 @@ struct ordered_set
     }
 };
 
+
 void func()
 {
-    newint(n);
+    
 }
 int main()
 {
